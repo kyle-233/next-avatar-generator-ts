7eebbad1f63cbf1fb4b80752f7cf595a0f172f20
kyle-233 <lubingfu@chinabzc.com>
2024-08-10 14:35:37 +0800

    feat: add footer separator

--- app/[lang]/(main)/footer.tsx
+++ app/[lang]/(main)/footer.tsx
@@ -1,10 +1,12 @@
 import { LanguageSwitcher } from '@/components/language-switch'
 import { ModeToggle } from '@/components/theme-toggle'
+import { Separator } from '@/components/ui/separator'
 
 export const Footer = () => {
   return (
     <footer className="flex justify-center items-center gap-4 py-4  ">
       <LanguageSwitcher />
+      <Separator orientation="vertical" className="h-4 w-[2px] rounded-sm" />
       <ModeToggle />
     </footer>
   )
